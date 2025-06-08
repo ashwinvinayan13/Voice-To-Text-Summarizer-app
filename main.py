f49369a35a85f290db0e3ec1d88df326413c89f3
ashwinvinayan13 <ashwinvinayan1306@gmail.com>
2025-06-08 22:53:04 +0530

    Updated Main file with text to transcription

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from transformers import pipeline
+from fastapi import UploadFile, File
 
 asr = pipeline("automatic-speech-recognition", model="openai/whisper-base")
 summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
@@ -21,5 +22,11 @@ async def home():
 
 @app.post("/summary")
 async def summarize(data: TextInput):
-    result = summarizer(data.summary.strip(), min_length = 10, max_length = 60)
+    result = summarizer(data.summary.strip(), min_length=10, max_length=60)
     return {"summary": result[0]['summary_text']}
+
+@app.post("/transcribe")
+async def transcribe_audio(file: UploadFile = File(...)):
+    audio_bytes = await file.read()
+    result = asr(audio_bytes, return_timestamps=False, generate_kwargs={"language":"en"}, chunk_length_s=30)
+    return {"transcription": result["text"]}
